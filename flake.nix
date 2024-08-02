b48b4e89dd83839f0d58b224248453bbfdf0f527
Symon Vitalek <symonvitalik@gmail.com>
2024-08-01 21:54:57 -0400

    YES

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     inputs =
         {
             flake-utils.url = "github:numtide/flake-utils?rev=b1d9ab70662946ef0850d488da1c9019f3a9752a" ;
-            nixpkgs.url = "github:NixOs/nixpkgs?rev=b1d9ab70662946ef0850d488da1c9019f3a9752a" ;
+            nixpkgs.url = "github:NixOs/nixpkgs?rev=9afce28a1719e35c295fe8b379a491659acd9cd6" ;
         } ;
     outputs =
         { flake-utils , nixpkgs , self } :
@@ -10,11 +10,40 @@
                 fun =
                     system :
                         let
-                            lib = null ;
-                            pkgs = builtins.import nixpkgs { system = system ; } ;
+                            lib =
+                                { scripts } : { pkgs , ... } @secondary :
+                                    let
+                                        arguments =
+                                            {
+                                                scripts =
+                                                    let
+                                                        mapper =
+                                                            path : name : value :
+                                                                if builtins.typeOf value == "lambda" then pkgs.writeShellScript name ( value secondary )
+                                                                else builtins.mapAttrs ( builtins.concatLists [ path [ name ] ] ) ;
+                                                        in builtins.mapAttrs ( mapper [ ] ) scripts ;
+                                            } ;
+                                        in
+                                            {
+                                                scripts = arguments.scripts ;
+                                            } ;
+                            pkgs = import nixpkgs { system = system ; } ;
                             in
                                 {
-                                    checks = { } ;
+                                    checks =
+                                        {
+                                            simple =
+                                                pkgs.stdenv.mkDerivation
+                                                    {
+                                                        name = "simple" ;
+                                                        src = ./. ;
+                                                        buildCommand =
+                                                            ''
+                                                                ${ pkgs.coreutils }/bin/echo THIS ALWAYS FAILS &&
+                                                                    exit 64
+                                                            '' ;
+                                                    } ;
+                                        } ;
                                     lib = lib ;
                                 } ;
                 in flake-utils.lib.eachDefaultSystem fun ;
