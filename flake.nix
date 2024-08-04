1a533fdc249324b2aac90d9e3caf42584b03154c
Symon Vitalek <symonvitalik@gmail.com>
2024-08-04 12:08:31 -0400

    WTF cleanup is having problems

--- flake.nix
+++ flake.nix
@@ -78,9 +78,6 @@
                                                                                             ${ pkgs.coreutils }/bin/echo AAA 0002100 >> /tmp/AAAA
                                                                                         ### AAAA
                                                                                             ${ pkgs.coreutils }/bin/echo ${ pkgs.coreutils }/bin/nice --adjustment 19 ${ pkgs.writeShellScript "release" release } ${ environment-variable "RESOURCE" } ${ environment-variable "TARGET_PID" } | ${ at } now > /dev/stderr 2>1
-                                                                                        ### AAAA
-                                                                                            ${ pkgs.coreutils }/bin/echo AAA 0002110 >> /tmp/AAAA
-                                                                                        ### AAAA
                                                                                                 ${ pkgs.coreutils }/bin/echo ${ environment-variable target }
                                                                                         else
                                                                                         ### AAAA
