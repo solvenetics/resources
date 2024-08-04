87e5acd6e56a5751e2c848f886608834cb4269df
Symon Vitalek <symonvitalik@gmail.com>
2024-08-04 10:35:37 -0400

    better optics?

--- flake.nix
+++ flake.nix
@@ -295,8 +295,8 @@
                                                                                                                                 ${ pkgs.coreutils }/bin/echo outer missing init flag &&
                                                                                                                                     exit 64
                                                                                                                             fi &&
-                                                                                                                            ${ pkgs.coreutils }/bin/echo We have determined that sleep 0.001 seconds is too fast because the test for removal happens before the removal has had a chance. &&
-                                                                                                                            ${ pkgs.coreutils }/bin/sleep 0.01s &&
+                                                                                                                            ${ pkgs.coreutils }/bin/echo We have determined that sleep 0.01 seconds is too fast because the test for removal happens before the removal has had a chance. &&
+                                                                                                                            ${ pkgs.coreutils }/bin/sleep 0.1s &&
                                                                                                                             if [ ! -f ${ environment-variable "RELEASE_FLAG" } ]
                                                                                                                             then
                                                                                                                                 ${ pkgs.coreutils }/bin/echo outer missing release flag >&2 &&
