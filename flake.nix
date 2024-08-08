8753b37e23107d9857497eb50afbefe68d135605
Symon Vitalek <symonvitalik@gmail.com>
2024-08-08 07:51:01 -0400

    wtf

--- flake.nix
+++ flake.nix
@@ -224,11 +224,12 @@
                                                                                                                     trap cleanup EXIT
                                                                                                                     export TEMPORARY=${ environment-variable 1 } &&
                                                                                                                     export INIT_STATUS=${ environment-variable 2 } &&
-                                                                                                                    export RELEASE_STATUS=${ environment-variable 3 } &&
-                                                                                                                    export ARGUMENTS=${ environment-variable 4 } &&
-                                                                                                                    export STDIN=${ environment-variable 5 } &&
-                                                                                                                    export HAS_STDIN=${ environment-variable 6 } &&
-                                                                                                                    export TEST_INIT=${ environment-variable 7 } &&
+                                                                                                                    export INIT_GOOD=${ environment-variable 3 } &&
+                                                                                                                    export RELEASE_STATUS=${ environment-variable 4 } &&
+                                                                                                                    export ARGUMENTS=${ environment-variable 5 } &&
+                                                                                                                    export STDIN=${ environment-variable 6 } &&
+                                                                                                                    export HAS_STDIN=${ environment-variable 7 } &&
+                                                                                                                    export TEST_INIT=${ environment-variable 8 } &&
                                                                                                                     if [ "${ environment-variable "HAS_STDIN" }" == "true" ]
                                                                                                                     then
                                                                                                                         TARGET=$( ${ pkgs.coreutils }/bin/echo ${ environment-variable "STDIN" } | ${ environment-variable "TEMPORARY" } ${ environment-variable "ARGUMENTS" } )
@@ -318,11 +319,15 @@
                                                                                                                             fi &&
                                                                                                                             if [ ! -f ${ environment-variable "INIT_TARGET" } ]
                                                                                                                             then
-                                                                                                                                export MESSAGE="We did not create the INIT_TARGET." &&
+                                                                                                                                export MESSAGE="We did not create the init target." &&
                                                                                                                                     exit 64
-                                                                                                                            elif [ $( ${ pkgs.coreutils }/bin/cat ${ environment-variable "INIT_TARGET" } ) != ${ environment-variable "TARGET" } ]
+                                                                                                                            elif [ ${ environment-variable "INIT_GOOD" } == true ] && [ $( ${ pkgs.coreutils }/bin/cat ${ environment-variable "INIT_TARGET" } ) != ${ environment-variable "TARGET" } ]
                                                                                                                             then
-                                                                                                                                export MESSAGE="We did not correctly write the init target." &&
+                                                                                                                                export MESSAGE="We did not correctly create the init target." &&
+                                                                                                                                    exit 64
+                                                                                                                            elif [ ${ environment-variable "INIT_GOOD" } == false ] && [ $( ${ pkgs.coreutils }/bin/cat ${ environment-variable "INIT_TARGET" } ) != ${ environment-variable "TARGET" } ]
+                                                                                                                            then
+                                                                                                                                export MESSAGE="We did not correctly create the init target." &&
                                                                                                                                     exit 64
                                                                                                                             fi
                                                                                                                     else
@@ -453,10 +458,10 @@
                                                                                     ${ resources.scripts.alpha } &&
                                                                                     exit 64
                                                                             fi &&
-                                                                            ${ resources.scripts.verification.temporary } ${ resources.temporary.beta-11 } 0 0 59eea253e2372353f978847b87e80d02b0568754c503e3718bbc8388ee99bf7381479ca8a2935362188f581cdab6ffb59dc403381b59d66ae1d62eb4802d93f4 5127cbcfc550b084ca27070a3d5b4aeb034cb174fd9aedb19f9e3c85c95f97d138123ca6b826fd5d009e9f24e1c25d6aedefc8c91f92b8284fae94942a488c9d true true &&
-                                                                            ${ resources.scripts.verification.temporary } ${ resources.temporary.beta-11 } 0 0 c8a2d7e7f7683f8f2db452bf311013d17d321a077489e4928f1a95d38a26a5b99942c2b69608238c31816eba23369bab3f43f51c7eb1c954bcaa56a7898d3886 47ce8fce72162158377951a30e52a638c2dd87b849d88ce4e4d65622ecda0fcffde884831cd1cca3ad03e46b7bf3cceb3136bcff9b8c55461567c29d20292657 false true &&
-                                                                            ${ resources.scripts.verification.temporary } ${ resources.temporary.beta-21 } 65 0 57e593f977b1be52e9bfdc465811aa7ade6d6d99b202e64fb0a4d0f5bc9ae581244a7eba872cd073ff9bbd374282421ff24590d703d75d4b82596811531344d7 c1cdefe06092f250e1a05013e2d78957927cb865300fb03b86a2788c812f56a29cf074a7d7291b17c965ddddc6f1b7c9d99885a4827a925b5d72cf1b9bb81191 true true &&
-                                                                            ${ resources.scripts.verification.temporary } ${ resources.temporary.beta-21 } 65 0 a650fec07ebe71e3bd0cc888f03bbb023c11b6cd0a5565d8ed579e899ba40f100e83f24feb9043d1df8f764bc30a70b752520bb79a03daac773af921cffa6021 891e854719b25e5610aadac6bd2e7351fcfbdf489a1a07701a743e5b381d870be1d49e1f7b49fb7dd7d916daa203c2fc4e27593295933532980de39938e27a5c false true
+                                                                            ${ resources.scripts.verification.temporary } ${ resources.temporary.beta-11 } 0 true 0 59eea253e2372353f978847b87e80d02b0568754c503e3718bbc8388ee99bf7381479ca8a2935362188f581cdab6ffb59dc403381b59d66ae1d62eb4802d93f4 5127cbcfc550b084ca27070a3d5b4aeb034cb174fd9aedb19f9e3c85c95f97d138123ca6b826fd5d009e9f24e1c25d6aedefc8c91f92b8284fae94942a488c9d true true &&
+                                                                            ${ resources.scripts.verification.temporary } ${ resources.temporary.beta-11 } 0 true 0 c8a2d7e7f7683f8f2db452bf311013d17d321a077489e4928f1a95d38a26a5b99942c2b69608238c31816eba23369bab3f43f51c7eb1c954bcaa56a7898d3886 47ce8fce72162158377951a30e52a638c2dd87b849d88ce4e4d65622ecda0fcffde884831cd1cca3ad03e46b7bf3cceb3136bcff9b8c55461567c29d20292657 false true &&
+                                                                            ${ resources.scripts.verification.temporary } ${ resources.temporary.beta-21 } 65 false 0 57e593f977b1be52e9bfdc465811aa7ade6d6d99b202e64fb0a4d0f5bc9ae581244a7eba872cd073ff9bbd374282421ff24590d703d75d4b82596811531344d7 c1cdefe06092f250e1a05013e2d78957927cb865300fb03b86a2788c812f56a29cf074a7d7291b17c965ddddc6f1b7c9d99885a4827a925b5d72cf1b9bb81191 true true &&
+                                                                            ${ resources.scripts.verification.temporary } ${ resources.temporary.beta-21 } 65 false 0 a650fec07ebe71e3bd0cc888f03bbb023c11b6cd0a5565d8ed579e899ba40f100e83f24feb9043d1df8f764bc30a70b752520bb79a03daac773af921cffa6021 891e854719b25e5610aadac6bd2e7351fcfbdf489a1a07701a743e5b381d870be1d49e1f7b49fb7dd7d916daa203c2fc4e27593295933532980de39938e27a5c false true
                                                                     '' ;
                                                     } ;
                                         } ;
