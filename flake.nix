b563497a208a9dbfafb8129cd9790932cb3bb84f
Symon Vitalek <symonvitalik@gmail.com>
2024-08-07 20:00:46 -0400

    wtf

--- flake.nix
+++ flake.nix
@@ -235,6 +235,15 @@
                                                                                                                                     export OBSERVED="$( ${ pkgs.coreutils }/bin/cat ${ environment-variable "RESOURCE" }/init.err.log )" &&
                                                                                                                                     export EXPECTED="193c8f5b2f5b97ba3ed5cd30c625144f71a361d8f9b225ae6614725ea1b59a8de3d995628902ca8fa5a5d4bb4376258302538eb922d2283fc7894dda1ffa8952" &&
                                                                                                                                     exit 64
+                                                                                                                            fi &&
+                                                                                                                            if [ ! -f ${ environment-variable "INIT_ARGUMENTS" } ]
+                                                                                                                            then
+                                                                                                                                export MESSAGE="We did not write init arguments." &&
+                                                                                                                                    exit 64
+                                                                                                                            elif [ "$( ${ pkgs.coreutils }/bin/cat ${ environment-variable "INIT_ARGUMENTS" } )" != "${ environment-variable "ARGUMENTS" }" ]
+                                                                                                                            then
+                                                                                                                                export MESSAGE="We did not correctly write the init arguments." &&
+                                                                                                                                    exit 54
                                                                                                                             fi
                                                                                                                     else
                                                                                                                         if [ -e ${ environment-variable "TARGET" } ]
@@ -251,8 +260,13 @@
                                                                                                                             then
                                                                                                                                 export MESSAGE="We did log init err." &&
                                                                                                                                     exit 64
+                                                                                                                            fi &&
+                                                                                                                            if [ -e ${ environment-variable "INIT_ARGUMENTS"  }]
+                                                                                                                            then
+                                                                                                                                export MESSAGE="We did write the arguments." &&
+                                                                                                                                    exit 64
                                                                                                                             fi
-                                                                                                                    fi
+                                                                                                                    fi &&
 
                                                                                                                     if [ -e ${ environment-variable "RELEASE_FLAG" } ]
                                                                                                                     then
