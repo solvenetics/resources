43b58ba235907ac01c28b32a0b25ac4aa909be84
Symon Vitalek <symonvitalik@gmail.com>
2024-08-11 16:03:39 -0400

    clued

--- flake.nix
+++ flake.nix
@@ -56,7 +56,13 @@
                                                                                         HAS_STANDARD_INPUT="false" &&
                                                                                             STANDARD_INPUT="" &&
                                                                                     fi &&
+                                                                                    ENCODED_ARGUMENTS=$( ${ pkgs.coreutils }/bin/echo ${ environment-variable "@" } | ${ pkgs.coreutils }/bin/base64 ) &&
+                                                                                    ENCODED_STANDARD_INPUT=$( ${ pkgs.coreutils }/bin/echo ${ environment-variable "STANDARD_INPUT" } | ${ pkgs.coreutils }/bin/base64 ) &&
+                                                                                    export ${ cache-epoch-hash }=$( ${ pkgs.coreutils }/bin/echo "${ constant-hash } ${ environment-variable "EPOCH_TIMESTAMP" } ${ environment-variable "@" } ${ environment-variable "HAS_STANDARD_INPUT" } ${ environment-variable "STANDARD_INPUT" } $( ${ pkgs.coreutils }/bin/whoami )" | ${ pkgs.coreutils }/bin/sha512 sum | ${ pkgs.coreutils }/bin/cut --bytes -128 ) &&
+                                                                                    exec 201> ${ cache-directory }/${ environment-variable cache-epoch-hash }.lock &&
 
+                                                                                    ${ pkgs.coreutils }/bin/ln --symbolic
+                                                                                    ${ pkgs.coreutil }/bin/rm ${ environment-variable "CACHE_DIRECTORY" }.lock &&
                                                                                     true
                                                                             '' ;
                                                                         constant-hash = builtins.hashString "sha512" ( builtins.concatStringsSep ";" ( builtins.concatLists [ path [ name ( builtins.toString temporary ) ] ] ) ) ;
